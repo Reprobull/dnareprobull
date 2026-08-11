8ebbed5285ed9bf57e67c86fc40d4ce8afd3aa8f
Reprobull <marcosguerra@usp.br>
2026-08-11 00:55:36 -0300

    Delete middleware.ts

--- middleware.ts
+++
@@ -1,6 +0,0 @@
-export { auth as middleware } from "@/lib/auth";
-
-export const config = {
-  // Protege todas as rotas, exceto login, arquivos estáticos e a própria API de auth
-  matcher: ["/((?!login|api/auth|_next/static|_next/image|favicon.ico).*)"],
-};
